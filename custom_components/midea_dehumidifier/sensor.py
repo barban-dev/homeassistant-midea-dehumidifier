1632bc2767cb38ebbc34b0a102b1d7ac79c52bb4
Andrea Barbaresi <barban.mobile@gmail.com>
2024-08-31 20:06:33 +0200

    code cleaning

--- custom_components/midea_dehumidifier/sensor.py
+++ custom_components/midea_dehumidifier/sensor.py
@@ -3,20 +3,15 @@ Custom integation based on humidifer and sensor platforms for EVA II PRO WiFi Sm
 For more details please refer to the documentation at
 https://github.com/barban-dev/midea_inventor_dehumidifier
 """
-VERSION = '1.0.4'
+VERSION = '1.0.5'
 
 import logging
 from custom_components.midea_dehumidifier import DOMAIN, MIDEA_TARGET_DEVICE
 from homeassistant.helpers.entity import Entity
-
-#patch for HA2024.1.0
-#from homeassistant.const import (DEVICE_CLASS_HUMIDITY)
 from homeassistant.components.sensor import SensorDeviceClass
-
 from custom_components.midea_dehumidifier.humidifier import ATTR_CURRRENT_HUMIDITY
 
 _LOGGER = logging.getLogger(__name__)
-
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up available sensors for MideaDehumidifier Humidifier Entity."""
@@ -48,11 +43,7 @@ class MideaDehumidifierSensor(Entity):
         self._hass = hass
         self._name = 'midea_dehumidifier_' + targetDevice['id'] + '_humidity'
         self._unique_id = 'midea_dehumidifier_' + targetDevice['id'] + '_humidity'
-
-        #patch for HA2024.1.0
-        #self._device_class = DEVICE_CLASS_HUMIDITY
         self._device_class = SensorDeviceClass.HUMIDITY
-
         self._unit_of_measurement = '%'
         self._icon = 'mdi:water-percent'
         #self._battery = battery
